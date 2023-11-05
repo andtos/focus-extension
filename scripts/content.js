307d2eaa0d163a361bc12a748092ad791bf05d18
ast8705 <andtoskas1104@gmail.com>
2023-11-05 14:27:38 -0500

    done

--- scripts/content.js
+++ scripts/content.js
@@ -1,26 +1,25 @@
 function timeOut(){
-    const customLoaderHtml = `
-    <iframe id="custom-loader-extension">
-    hello
+    const screenBlocker = `
+    <iframe id="content">
     </iframe>`;
 
-    const loaderContainer = document.createElement("div");
-    loaderContainer.innerHTML = customLoaderHtml;
-    document.documentElement.appendChild(loaderContainer);
+    const screenBlockerContainer = document.createElement("body");
+    screenBlockerContainer.innerHTML = screenBlocker;
+    document.documentElement.appendChild(screenBlockerContainer);
 
-    const div = loaderContainer.querySelector("iframe");
-    div.style.position = "fixed";
-    div.style.top = "0";
-    div.style.left = "0";
-    div.style.width = "100%";
-    div.style.height = "100%";
-    div.style.border = "none";
-    div.style.backgroundColor = "black";
-    div.style.zIndex = 1000;
+    const element = screenBlockerContainer.querySelector("iframe");
+    element.style.position = "fixed";
+    element.style.top = "0";
+    element.style.left = "0";
+    element.style.width = "1000%";
+    element.style.height = "1000%";
+    element.style.backgroundColor = "black";
+    element.style.zIndex = 1000;
 
     setTimeout(() => {
-    loaderContainer.style.display = "none";
-    }, 5000);
+    screenBlockerContainer.style.display = "none";
+
+    }, 10000);
 }
 
 timeOut();
